5b8354148454a17cfd48997f1eb6320d222aef50
Phoneix369 <mervesevim050@gmail.com>
2024-10-12 22:19:48 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <ctype.h>
-#include "classes.h"
-#include "functions.h"
-
-int main(){
-
-
-    char senaryoFile[] = "2.json";
-
-    Takim insan_imparatorlugu = {"insan_imparatorlugu"};
-    Takim ork_legi = {"ork_legi"};
-
-    readTakim(&insan_imparatorlugu,senaryoFile);
-    readTakim(&ork_legi,senaryoFile);
-
-
-    return 0;
-}
-
-
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <stdbool.h> 
+#include <string.h> 
+#include <ctype.h> 
+#include "classes.h" 
+#include "functions.h" 
+ 
+ 
+ 
+int main(){ 
+ 
+ 
+    char* senaryoFile = "9.json"; 
+ 
+    Takim insan_imparatorlugu = {"insan_imparatorlugu"}; 
+ 
+    Takim ork_legi = {"ork_legi"}; 
+ 
+    readTakim(&insan_imparatorlugu,senaryoFile); 
+    printf("\n\n\n\n\n\n\n----------------------------------------------------------------------------\n\n\n\n\n\n\n"); 
+    readTakim(&ork_legi,senaryoFile); 
+ 
+ 
+    system("pause"); 
+ 
+ 
+    return 0; 
+} 
+ 
+ 
 //mun ?   http req ?
