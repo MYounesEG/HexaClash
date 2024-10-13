66028ef042f4196fc4875c0cbf9ed5c6efe97cec
Yunus Aboushewil <158834031+MYounesEG@users.noreply.github.com>
2024-10-13 17:47:28 +0300

    used downloadFile and deleteFile functions , make the user selecting the scenario

--- main.c
+++ main.c
@@ -1,32 +1,31 @@
-#include <stdlib.h> 
-#include <stdio.h> 
-#include <stdbool.h> 
-#include <string.h> 
-#include <ctype.h> 
-#include "classes.h" 
-#include "functions.h" 
- 
- 
- 
-int main(){ 
- 
- 
-    char* senaryoFile = "9.json"; 
- 
-    Takim insan_imparatorlugu = {"insan_imparatorlugu"}; 
- 
-    Takim ork_legi = {"ork_legi"}; 
- 
-    readTakim(&insan_imparatorlugu,senaryoFile); 
-    printf("\n\n\n\n\n\n\n----------------------------------------------------------------------------\n\n\n\n\n\n\n"); 
-    readTakim(&ork_legi,senaryoFile); 
- 
- 
-    system("pause"); 
- 
- 
-    return 0; 
-} 
- 
- 
-//mun ?   http req ?
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include "classes.h"
+#include "functions.h"
+
+int main()
+{
+    int SenarioNumber;
+    char senaryoFile[10] = {0};
+    printf("Enter Nerario number : ");
+    scanf("%d",&SenarioNumber);
+
+    downloadFile(SenarioNumber,senaryoFile);
+
+    Takim insan_imparatorlugu = {"insan_imparatorlugu"};
+
+    Takim ork_legi = {"ork_legi"};
+
+    readTakim(&insan_imparatorlugu,senaryoFile);
+    printf("\n\n\n\n\n\n\n----------------------------------------------------------------------------\n\n\n\n\n\n\n");
+    readTakim(&ork_legi,senaryoFile);
+
+
+    deleteFile(senaryoFile);
+
+
+    return 0;
+}
